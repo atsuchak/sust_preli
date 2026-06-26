2759f13065f1fc9a6ed41e1d4a8dd20ef6d0560f
atsuchak <suchak0630@gmail.com>
2026-06-26 20:06:41 +0600

    chore: initialize spring boot base project

---
+++ queuestorm-investigator/src/main/java/com/queuestorm/investigator/QueueStormInvestigatorApplication.java
@@ -0,0 +1,13 @@
+package com.queuestorm.investigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueStormInvestigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueStormInvestigatorApplication.class, args);
+	}
+
+}
---
+++ queuestorm-investigator/src/test/java/com/queuestorm/investigator/QueueStormInvestigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queuestorm.investigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueStormInvestigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
